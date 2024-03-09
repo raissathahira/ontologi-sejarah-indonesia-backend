11c00259dfd69883f7b3c082b7bc20c083770224
ariiesaka <judahutauruk12@gmail.com>
2024-03-09 12:40:29 +0700

    Initial setup for Django project

---
+++ ontologi_sejarah_indonesia/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ontologi_sejarah_indonesia project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ontologi_sejarah_indonesia.settings')
+
+application = get_asgi_application()
