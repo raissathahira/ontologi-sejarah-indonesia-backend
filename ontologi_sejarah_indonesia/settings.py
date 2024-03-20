1ac42cdef0fdaa7bc033704b528f02f96660803a
unknown <richie.christiansen@ui.ac.id>
2024-03-20 12:47:15 +0700

    backend graph

--- ontologi_sejarah_indonesia/settings.py
+++ ontologi_sejarah_indonesia/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
+    'graph'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CSRF_TRUSTED_ORIGINS = ['*']
