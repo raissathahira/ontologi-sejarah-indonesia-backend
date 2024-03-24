9b6e8e30593306fbbe24923df6e9b8a3c299e913
ariiesaka <judahutauruk12@gmail.com>
2024-03-25 01:22:22 +0700

    Implement more GET API for detail page

--- map/views.py
+++ map/views.py
@@ -2,20 +2,10 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 from django.http import JsonResponse
 from shapely import wkt
 from shapely.geometry import mapping, shape
-from .queries import military_conflict
+from shapely.ops import unary_union
+from .queries import prefix, get_types, event, military_conflict
 
-prefix = """
-prefix :      <http://127.0.0.1:3333/> 
-prefix foaf:  <http://xmlns.com/foaf/0.1/> 
-prefix geo:   <http://www.opengis.net/ont/geosparql#> 
-prefix owl:   <http://www.w3.org/2002/07/owl#> 
-prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
-prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> 
-prefix sem:   <http://semanticweb.cs.vu.nl/2009/11/sem/> 
-prefix time:  <http://www.w3.org/2006/time#> 
-prefix vcard: <http://www.w3.org/2006/vcard/ns#> 
-prefix xsd:   <http://www.w3.org/2001/XMLSchema#> 
-"""
+base_prefix = "http://127.0.0.1:3333/"
 
 def fetch_data(request):
     query = prefix + """
@@ -45,15 +35,14 @@ select DISTINCT ?event ?label ?lat ?lon ?dateStart ?dateEnd where {
     results = sparql.query().convert()
     
     data = []
-    
-    base_prefix = "http://127.0.0.1:3333/"
+
     
     for result in results["results"]["bindings"]:
         data.append({
             "iri": result["event"]["value"][len(base_prefix):],
             "name": result["label"]["value"],
-            "yearStart": int(result["dateStart"]["value"][-4:]),
-            "yearEnd": int(result["dateEnd"]["value"][-4:]),
+            "yearStart": int(result["dateStart"]["value"][:4]),
+            "yearEnd": int(result["dateEnd"]["value"][:4]),
             "latitude": float(result["lat"]["value"]),
             "longitude": float(result["lon"]["value"]),
         })
@@ -62,7 +51,12 @@ select DISTINCT ?event ?label ?lat ?lon ?dateStart ?dateEnd where {
 
 
 def get_detail(request, iri):
-    query = (prefix + military_conflict).format(iri)
+    type_func = {
+        "Event": get_event_detail,
+        "Military Conflict": get_military_conflict_detail
+    }
+    
+    query = (prefix + get_types).format(iri)
 
     sparql = SPARQLWrapper("http://localhost:7200/repositories/indonesian-history-ontology")
     sparql.setQuery(query)
@@ -70,33 +64,80 @@ def get_detail(request, iri):
 
     results = sparql.query().convert()
     result = results["results"]["bindings"][0]
-    geojson = mapping(wkt.loads(result["location"]["value"])) if "location" in result else None
     
-    detail = get_military_conflict_detail(result)
-        
-    data = {
-        "name": result["label"]["value"],
-        "geojson": geojson,
-        "bounds": get_bound(geojson),
-        "detail": detail
-    }
+    types = result["typeLabels"]["value"].split(",")
     
-    return JsonResponse(data, safe=False)
+    detail = {}
+    detail["detail"] = {}
+    
+    for type_, func in type_func.items():
+        if type_ in types:
+            func(iri, detail)
+    
+    return JsonResponse(detail, safe=False)
 
-def get_bound(geojson):
-    if geojson is None:
+def get_largest_bound(geojson_list):
+    if geojson_list is None:
         return None
     
-    bounds = shape(geojson).bounds
+    shapes = [shape(geojson) for geojson in geojson_list]
+    
+    multi_poly = unary_union(shapes)
+    
+    bounds = shape(multi_poly).bounds
     
     return [
         [bounds[1], bounds[0]],
         [bounds[3], bounds[2]]
     ]
     
-def get_military_conflict_detail(result):
-    keys = ["label", "result", "combatants1", "combatants2", "commanders1", "commanders2", "strength1", 
-            "strength2", "casualties1", "casualties2", "dateStart", "dateEnd"]
-    return {key: result[key]["value"] if key in result else None for key in keys}
+def get_event_detail(iri, detail):
+    query = (prefix + event).format(iri)
 
+    sparql = SPARQLWrapper("http://localhost:7200/repositories/indonesian-history-ontology")
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
 
+    results = sparql.query().convert()
+    result = results["results"]["bindings"]
+    
+    detail["detail"]["name"] = ("Nama", result[0]["label"]["value"])
+    detail["detail"]["dateStart"] = ("Tanggal mulai", result[0]["dateStart"]["value"])
+    detail["detail"]["dateEnd"] = ("Tanggal selesai", result[0]["dateEnd"]["value"])
+    
+    
+    detail["detail"]["feature"] = ("Tempat kejadian", [
+        (res["feature"]["value"][len(base_prefix):], res["featureLabel"]["value"])
+        for res in result
+    ])
+    
+    detail["location"] = [mapping(wkt.loads(res["location"]["value"])) for res in result]
+    
+    detail["bounds"] = get_largest_bound(detail["location"]) 
+     
+    
+def get_military_conflict_detail(iri, detail):
+    query = (prefix + military_conflict).format(iri)
+    
+    sparql = SPARQLWrapper("http://localhost:7200/repositories/indonesian-history-ontology")
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+
+    results = sparql.query().convert()
+    result = results["results"]["bindings"][0]
+    
+    multivalued_attr = ["combatants1", "combatants2", "commanders1", "commanders2"]
+    multivalued_label = ["Pihak 1", "Pihak 2", "Pemimpin pihak 1", "Pemimpin pihak 2"]
+
+    for i in range(len(multivalued_attr)):
+        if multivalued_attr[i] in result:
+            detail["detail"][multivalued_attr[i]] = (multivalued_label[i], [
+                (iri[len(base_prefix):], label)
+                for iri, label in zip(result[multivalued_attr[i]]["value"].split(","), result[multivalued_attr[i] + "Label"]["value"].split(","))
+            ])
+
+    detail["detail"]["strength1"] = ("Kekuatan pihak 1", result["strength1"]["value"] if "strength1" in result else None)
+    detail["detail"]["strength2"] = ("Kekuatan pihak 2", result["strength2"]["value"] if "strength2" in result else None)
+    detail["detail"]["casualties1"] = ("Korban pihak 1", result["casualties1"]["value"] if "casualties1" in result else None)
+    detail["detail"]["casualties2"] = ("Korban pihak 2", result["casualties2"]["value"] if "casualties2" in result else None)
+    detail["detail"]["causes"] = ("Penyebab", result["causes"]["value"] if "causes" in result else None)
