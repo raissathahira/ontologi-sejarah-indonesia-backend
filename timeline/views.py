7bdd3ffc94e6615c97ec41109069175d26b5059a
Michael <michaelbalma47@gmail.com>
2024-03-25 13:17:21 +0700

    add search filter

--- timeline/views.py
+++ timeline/views.py
@@ -3,37 +3,41 @@ from django.http import JsonResponse
 
 
 def fetch_data(request):
-    query = """
-prefix :      <http://localhost:3333/> 
-prefix foaf:  <http://xmlns.com/foaf/0.1/> 
-prefix geo:   <http://www.opengis.net/ont/geosparql#> 
-prefix owl:   <http://www.w3.org/2002/07/owl#> 
-prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
-prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> 
-prefix sem:   <http://semanticweb.cs.vu.nl/2009/11/sem/> 
-prefix time:  <http://www.w3.org/2006/time#> 
-prefix vcard: <http://www.w3.org/2006/vcard/ns#> 
-prefix xsd:   <http://www.w3.org/2001/XMLSchema#> 
+    name = request.GET.get('filter[name]', '')
 
-select ?baseURI ?actor ?label ?lat ?lon ?dateStart ?dateEnd where {
-    ?actor rdf:type sem:Actor ;
-    	rdfs:label ?label ;
-    	:location ?feature .
-    
-    ?feature geo:hasGeometry ?geometry .
-    ?geometry :latitude ?lat ;
-    	:longitude ?lon .
-    
-    ?actor time:hasTime ?tempEntity .
-    ?tempEntity time:hasBeginning ?inst1 ;
-    	time:hasEnd ?inst2 .
-    
-    ?inst1 time:inXSDDate ?dateStart .
-    ?inst2 time:inXSDDate ?dateEnd .
-    
-     BIND(REPLACE(STR(?actor), "([^:/]+://[^/]+/).*", "$1") AS ?baseURI)
-}
-"""
+    query = """
+        prefix :      <http://localhost:3333/>
+        prefix foaf:  <http://xmlns.com/foaf/0.1/>
+        prefix geo:   <http://www.opengis.net/ont/geosparql#>
+        prefix owl:   <http://www.w3.org/2002/07/owl#>
+        prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
+        prefix sem:   <http://semanticweb.cs.vu.nl/2009/11/sem/>
+        prefix time:  <http://www.w3.org/2006/time#>
+        prefix vcard: <http://www.w3.org/2006/vcard/ns#>
+        prefix xsd:   <http://www.w3.org/2001/XMLSchema#>
+        select ?baseURI ?actor ?label ?lat ?lon ?dateStart ?dateEnd where {
+            ?actor rdf:type sem:Actor ;
+            rdfs:label ?label ;
+            :location ?feature .
+
+            ?feature geo:hasGeometry ?geometry .
+            ?geometry :latitude ?lat ;
+                :longitude ?lon .
+
+            ?actor time:hasTime ?tempEntity .
+            ?tempEntity time:hasBeginning ?inst1 ;
+                time:hasEnd ?inst2 .
+
+            ?inst1 time:inXSDDate ?dateStart .
+            ?inst2 time:inXSDDate ?dateEnd .
+
+            BIND(REPLACE(STR(?actor), "([^:/]+://[^/]+/).*", "$1") AS ?baseURI)
+            
+            FILTER regex(str(?label), "%s", "i") 
+             
+        }
+        """ % name
 
     sparql = SPARQLWrapper("http://localhost:7200/repositories/orde-lama")
     sparql.setQuery(query)
