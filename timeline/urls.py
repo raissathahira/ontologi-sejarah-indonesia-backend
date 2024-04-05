e9b51ca8296a9faa982ddcf6a3947cae6fd3d985
Michael <michaelbalma47@gmail.com>
2024-04-06 00:52:36 +0700

    restructuring timeline frontend

--- timeline/urls.py
+++ timeline/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from timeline.views import fetch_data_event, fetch_data_person, location
+from timeline.views import timeline, show_events
 
 app_name = 'timeline'
 
 urlpatterns = [
-    path('event/', fetch_data_event),
-    path('person/', fetch_data_person),
-    path('location/<str:name>/', location),
+    path('', timeline),
+    path('events/', show_events),
 
 ]
