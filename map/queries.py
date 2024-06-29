c8e9e2aef94d24ef68f5db2f2d3a5b994fe64bdb
Michael <michaelbalma47@gmail.com>
2024-06-29 21:29:29 +0700

    change query on actor's events

--- map/queries.py
+++ map/queries.py
@@ -52,7 +52,7 @@ select DISTINCT ?a ?label ?type ?summary ?firstDateYear where {{
 }}
 ORDER BY ?priority ASC(?label)
 LIMIT 10
-OFFSET {1}}
+OFFSET {1}
 """
 
 get_total_search = """
@@ -294,18 +294,21 @@ select DISTINCT ?baseURI ?thing ?label ?summary ?wikiurl ?image ?firstDateDay ?f
     }}.
     
     ?version time:hasTime ?tempEntity .
-    ?tempEntity time:hasBeginning ?inst1 ;
-                time:hasEnd ?inst2 .
-        
-    	OPTIONAL {{?inst1 time:inDateTime ?firstDate .}}
-        OPTIONAL {{?firstDate time:day ?firstDateDay.}}
-        OPTIONAL {{?firstDate time:month ?firstDateMonth.}}
-        OPTIONAL {{?firstDate time:year ?firstDateYear.}}
-
-        OPTIONAL {{?inst2 time:inDateTime ?secondDate .}}
+    ?tempEntity time:hasBeginning ?inst1 .
+    
+    ?inst1 time:inDateTime ?firstDate .
+    ?firstDate time:year ?firstDateYear .
+    
+    OPTIONAL {{
+        ?tempEntity time:hasEnd ?inst2.
+        ?inst2 time:inDateTime ?secondDate .
         OPTIONAL {{?secondDate time:day ?secondDateDay.}}
         OPTIONAL {{?secondDate time:month ?secondDateMonth.}}
         OPTIONAL {{?secondDate time:year ?secondDateYear.}}
+    }}
+        
+        OPTIONAL {{?firstDate time:day ?firstDateDay.}}
+        OPTIONAL {{?firstDate time:month ?firstDateMonth.}}
       
     BIND(REPLACE(STR(?version), "([^:/]+://[^/]+/).*", "$1") AS ?baseURI) .
     
