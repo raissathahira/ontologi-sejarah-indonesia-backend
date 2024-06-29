4792e1060c15bf86557eac54cea5aee7fb5c6631
ariiesaka <judahutauruk12@gmail.com>
2024-06-29 16:08:08 +0700

    Update map and detail query

--- map/views.py
+++ map/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from shapely import wkt
 from shapely.geometry import mapping, shape
 from shapely.ops import unary_union
-from .queries import prefix, get_map, get_all, get_search, get_total_search, get_types, event, actor, person, place, conflict, military_person
+from .queries import prefix, get_map, get_all, get_search, get_total_search, get_types, event, actor, person, place
 
 base_prefix = "http://127.0.0.1:3333/"
 graphdb = "http://localhost:7200/repositories/indonesia-history-ontology"
@@ -33,8 +33,7 @@ def fetch_map_data(request):
         data[latitude][longitude].append({
             "iri": result["event"]["value"][len(base_prefix):],
             "name": result["label"]["value"],
-            "yearStart": int(result["dateStart"]["value"][:4]),
-            "yearEnd": int(result["dateEnd"]["value"][:4])
+            "yearStart": int(result["yearStart"]["value"]),
         })
 
     return JsonResponse(data, safe=False)
@@ -103,8 +102,6 @@ def fetch_total_search(request, search):
 def get_detail(request, iri):
     type_func = {
         "Event": get_event_detail,
-        "Conflict": get_conflict_detail,
-        "Military Person": get_military_person_detail,
         "Actor": get_actor_detail,
         "Feature": get_place_detail,
         "Person": get_person_detail,
@@ -163,10 +160,32 @@ def get_event_detail(iri, detail):
         return
 
     detail["detail"]["name"] = ("Nama", result["label"]["value"])
-    detail["detail"]["dateStart"] = (
-        "Tanggal mulai", format_date(result["dateStart"]["value"]))
-    detail["detail"]["dateEnd"] = (
-        "Tanggal selesai", format_date(result["dateEnd"]["value"]))
+    
+    startDate = format_date(
+        result["dayStart"]["value"] if "dayStart" in result else None,
+        result["monthStart"]["value"] if "monthStart" in result else None,
+        result["yearStart"]["value"] if "yearStart" in result else None
+    )
+    
+    if startDate is not None:
+        detail["detail"]["dateStart"] = (
+            "Tanggal mulai", startDate
+        )
+        
+    endDate = format_date(
+        result["dayEnd"]["value"] if "dayEnd" in result else None,
+        result["monthEnd"]["value"] if "monthEnd" in result else None,
+        result["yearEnd"]["value"] if "yearEnd" in result else None
+    )
+    
+    if endDate is not None:
+        detail["detail"]["dateEnd"] = (
+            "Tanggal selesai", format_date(
+                result["dayEnd"]["value"] if "dayEnd" in result else None,
+                result["monthEnd"]["value"] if "monthEnd" in result else None,
+                result["yearEnd"]["value"] if "yearEnd" in result else None
+            )
+        )
 
     multivalued_attr = ["actor", "person", "feature"]
     multivalued_label = ["Pihak terlibat", "Tokoh terlibat", "Lokasi kejadian"]
@@ -206,7 +225,7 @@ def get_place_detail(iri, detail):
     detail["detail"]["coordinate"] = (
         "Koordinat (dalam lat dan lng)", "(" + result[0]["lat"]["value"] + ", " + result[0]["lng"]["value"] + ")")
 
-    detail["detail"]["event"] = ("Daftar peristiwa", [
+    detail["detail"]["eventOccurred"] = ("Daftar peristiwa", [
         (res["event"]["value"][len(base_prefix):], res["eventLabel"]["value"])
         for res in result
     ])
@@ -229,6 +248,42 @@ def get_actor_detail(iri, detail):
 
     detail["image"] = result["image"]["value"] if "image" in result else None
     detail["detail"]["name"] = ("Nama", result["label"]["value"])
+    
+    startDate = format_date(
+        result["dayStart"]["value"] if "dayStart" in result else None,
+        result["monthStart"]["value"] if "monthStart" in result else None,
+        result["yearStart"]["value"] if "yearStart" in result else None
+    )
+    
+    if startDate is not None:
+        detail["detail"]["dateStart"] = (
+            "Tanggal berdiri", startDate
+        )
+        
+    endDate = format_date(
+        result["dayEnd"]["value"] if "dayEnd" in result else None,
+        result["monthEnd"]["value"] if "monthEnd" in result else None,
+        result["yearEnd"]["value"] if "yearEnd" in result else None
+    )
+    
+    if endDate is not None:
+        detail["detail"]["dateEnd"] = (
+            "Tanggal berakhir", format_date(
+                result["dayEnd"]["value"] if "dayEnd" in result else None,
+                result["monthEnd"]["value"] if "monthEnd" in result else None,
+                result["yearEnd"]["value"] if "yearEnd" in result else None
+            )
+        )
+        
+    multivalued_attr = ["event"]
+    multivalued_label = ["Terlibat dalam"]
+    
+    for i in range(len(multivalued_attr)):
+        if result[multivalued_attr[i]]['value']:
+            detail["detail"][multivalued_attr[i]] = (multivalued_label[i], [
+                (iri[len(base_prefix):], label)
+                for iri, label in zip(result[multivalued_attr[i]]["value"].split(","), result[multivalued_attr[i] + "Label"]["value"].split(","))
+            ])
             
 def get_person_detail(iri, detail):
     query = (prefix + person).format(iri)
@@ -241,18 +296,15 @@ def get_person_detail(iri, detail):
     results = sparql.query().convert()
     result = results["results"]["bindings"][0]
     
-    detail["detail"]["dateStart"] = (
-            "Lahir", format_date(result["dateStart"]["value"]) if "dateStart" in result else None)
-    
-    if ("dateEnd" in result):
-        detail["detail"]["dateEnd"] = (
-            "Wafat", format_date(result["dateEnd"]["value"]))
+    if "dateStart" in detail["detail"]:
+        detail["detail"]["dateStart"] = ("Tanggal lahir", detail["detail"]["dateStart"][1])
         
-    detail["detail"]["religion"] = (
-        "Agama", result["religion"]["value"] if "religion" in result else None)
+    if "dateEnd" in detail["detail"]:
+        detail["detail"]["dateEnd"] = ("Tanggal kematian", detail["detail"]["dateEnd"][1])
     
-    multivalued_attr = ["parent", "spouse", "children", "relative", "event"]
-    multivalued_label = ["Orang tua", "Pasangan", "Anak", "Kerabat", "Terlibat dalam"]
+    
+    multivalued_attr = ["parents", "children", "relative"]
+    multivalued_label = ["Orang tua", "Anak", "Kerabat"]
     
     for i in range(len(multivalued_attr)):
         if result[multivalued_attr[i]]['value']:
@@ -260,74 +312,17 @@ def get_person_detail(iri, detail):
                 (iri[len(base_prefix):], label)
                 for iri, label in zip(result[multivalued_attr[i]]["value"].split(","), result[multivalued_attr[i] + "Label"]["value"].split(","))
             ])
-        else:
-            detail["detail"][multivalued_attr[i]] = (
-                multivalued_label[i], None)
 
-
-def get_conflict_detail(iri, detail):
-    query = (prefix + conflict).format(iri)
-
-    sparql = SPARQLWrapper(
-        graphdb)
-    sparql.setQuery(query)
-    sparql.setReturnFormat(JSON)
-
-    results = sparql.query().convert()
-    result = results["results"]["bindings"][0]
-
-    multivalued_attr = []
-    multivalued_label = []
-
-    for i in range(len(multivalued_attr)):
-        if result[multivalued_attr[i]]['value']:
-            detail["detail"][multivalued_attr[i]] = (multivalued_label[i], [
-                (iri[len(base_prefix):], label)
-                for iri, label in zip(result[multivalued_attr[i]]["value"].split(","), result[multivalued_attr[i] + "Label"]["value"].split(","))
-            ])
-        else:
-            detail["detail"][multivalued_attr[i]] = (
-                multivalued_label[i], None)
-
-    detail["detail"]["casualties"] = (
-        "Korban", result["casualties"]["value"] if "casualties" in result else None)
-    detail["detail"]["causes"] = (
-        "Penyebab", result["causes"]["value"] if "causes" in result else None)
-
-
-def get_military_person_detail(iri, detail):
-    query = (prefix + military_person).format(iri)
+def format_date(day, month, year):
+    if year is None:
+        return None
     
-    sparql = SPARQLWrapper(
-        graphdb)
-    sparql.setQuery(query)
-    sparql.setReturnFormat(JSON)
-
-    results = sparql.query().convert()
-    result = results["results"]["bindings"][0]
-
-    multivalued_attr = ["commands", "battles"]
-    multivalued_label = ["pasukan", "Pertempuran"]
-
-    for i in range(len(multivalued_attr)):
-        if multivalued_attr[i] in result:
-            detail["detail"][multivalued_attr[i]] = (multivalued_label[i], [
-                (iri[len(base_prefix):], label)
-                for iri, label in zip(result[multivalued_attr[i]]["value"].split(","), result[multivalued_attr[i] + "Label"]["value"].split(","))
-            ])
-            
-    detail["detail"]["name"] = ("Nama", result["label"]["value"])
-    detail["detail"]["laterwork"] = (
-        "Karya", result["laterwork"]["value"] if "laterwork" in result else None)
-    detail["detail"]["birthname"] = (
-        "nama lahir", result["birthname"]["value"] if "birthname" in result else None)
-    detail["detail"]["awards"] = (
-        "Penghargaan", result["awards"]["value"] if "awards" in result else None)
-
-def format_date(date):
-    year, month, day = map(int, date.split("-"))
-
-    months = {
+    date = ""
+    
+    if day is not None:
+        date += str(day) + " "
+    
+    month = {
         1: "Januari",
         2: "Februari",
         3: "Maret",
@@ -340,6 +335,9 @@ def format_date(date):
         10: "Oktober",
         11: "November",
         12: "Desember"
-    }
+    }.get(int(month) if month is not None else None, None)
+    
+    if month is not None:
+        date += month + " "
 
-    return f"{day} {months.get(month)} {year}"
+    return date + str(year)
