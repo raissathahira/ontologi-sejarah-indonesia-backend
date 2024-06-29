5dab6e3d0fc3144342b21a049e8145c19367d69f
ariiesaka <judahutauruk12@gmail.com>
2024-06-29 20:53:23 +0700

    Fix no location error for event detail

--- map/queries.py
+++ map/queries.py
@@ -248,7 +248,7 @@ where {{
     
     ?version rdf:type sem:View .
     ?version sem:accordingTo ?authority .
-    ?authority rdfs:label ?authorityLabel
+    ?authority rdfs:label ?authorityLabel .
     
     OPTIONAL {{
         ?version :hasLocation ?feature .
